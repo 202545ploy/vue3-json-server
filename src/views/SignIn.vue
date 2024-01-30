b2d2d724627dee8f12f4973992a6db82a1f639f3
Khunaiko <pasuaiko21089@gmail.com>
2024-01-30 15:53:37 +0700

    up

---
+++ src/views/SignIn.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <SignIn />
+    </div>
+</template>
+
+<script setup>
+import SignIn from '@/components/Signin/SignIn.vue';
+</script>
+
+<style lang="scss" scoped>
+
+</style>
